25a4fd2959f94b333d7202430290a0de4a2dc960
elisha <korosenseimark40@gmail.com>
2025-10-01 19:08:21 +0600

    makeing the home page with stock

--- components/userDropdown.tsx
+++ components/userDropdown.tsx
@@ -21,7 +21,7 @@ const UserDropdown = () => {
     router.push("/sign-in");
   }
 
-  const user = { name: 'John', email: 'contact@jsmastery.com' };
+  const user = { name: 'Elisha', email: 'contact@Elisha.com' };
 
   return (
     <DropdownMenu>
@@ -44,7 +44,7 @@ const UserDropdown = () => {
         <DropdownMenuLabel>
           <div className="flex relative items-center gap-3 py-2">
             <Avatar className="h-10 w-10">
-              <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" />
+              <AvatarImage src= "https://github.com/shadcn.png"/>
               <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
                 {user.name[0]}
               </AvatarFallback>
